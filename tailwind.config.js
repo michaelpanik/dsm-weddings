a919c21119cb7009fcad43b9177fa65a42d61c09
Michael Panik <michael@hellobordo.com>
2020-06-24 11:01:09 -0500

    Neutralize gray palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       letterSpacing: {
         wider: '.2em',
         widest: '.3em',
+      },
+      colors: {
+        gray: {
+          '100': '#FCFCFC',
+          '200': '#F7F7F7',
+          '300': '#F0F0F0',
+          '400': '#E0E0E0',
+          '500': '#BFBFBF',
+          '600': '#969696',
+          '700': '#696969',
+          '800': '#4A4A4A',
+          '900': '#2B2B2B',
+        }
       }
     },
   },
