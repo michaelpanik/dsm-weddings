d31b06aaca0317be60c6c3b777bf9d1d2e92aa33
Michael Panik <michael@michael-panik.com>
2022-05-18 12:20:24 -0500

    Add stream page

---
+++ src/pages/streams/[streamSlug].js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const StreamPage = () => {
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="About DSM" />
+      <Hero>
+        <h1 className="text-4xl uppercase tracking-widest text-white font-light">
+          Brianna + Preston
+        </h1>
+      </Hero>
+      <p>June 11, 2022</p>
+    </Layout>
+  )
+}
+
+export default StreamPage
